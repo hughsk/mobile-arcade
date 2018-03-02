4d84961529c491fc8d521b613533580387880b6c
tommygraven <tommygraven@me.com>
2018-03-02 22:59:39 +0000

    Removing useless functions from DeadZone class

--- Assets/_MobileArcade/Common/Scripts/DeadZone.cs
+++ Assets/_MobileArcade/Common/Scripts/DeadZone.cs
@@ -9,16 +9,6 @@ using UnityEngine;
 /// </summary>
 public class DeadZone : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
 	void OnTriggerEnter(Collider _col)
 	{
 		if (_col.gameObject.tag == "Player")
