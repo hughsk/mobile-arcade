55d436dcf3ca8d14ae44bda10c0e86cdeb62a085
tommygraven <tommygraven@me.com>
2018-04-09 14:19:05 +0100

    FitIn checks right amount of players

--- Assets/_MobileArcade/Common/Scripts/LevelManager.cs
+++ Assets/_MobileArcade/Common/Scripts/LevelManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 
 public class LevelManager : MonoBehaviour {
@@ -7,11 +8,17 @@ public class LevelManager : MonoBehaviour {
 	[SerializeField] Transform UICanvasPrefab;
 	[SerializeField] GameObject UICountdownPrefab;
 	[HideInInspector] public static bool isCountdownOver;
+	[Space(10)]
+
+	protected PlayerManager playerManager;
+	[HideInInspector] public Dictionary<string, Player> players;
 	
 	public virtual void OnEnable () {
 		Transform UICanvas = Instantiate(UICanvasPrefab);
 		Instantiate(UICountdownPrefab, UICanvas);
 		isCountdownOver = false;
 		Physics.gravity = new Vector3(0, -0.5f, 0);
+
+		playerManager = GetComponent<PlayerManager>();
 	}
 }
