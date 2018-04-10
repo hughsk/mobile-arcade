89853c1c5c1271b87b99469d350a4edf32c74cd8
tommygraven <tommygraven@me.com>
2018-04-10 22:55:57 +0100

    background scrolling

--- Assets/_MobileArcade/MinigameFitIn/Scripts/FitInBackground.cs
+++ Assets/_MobileArcade/MinigameFitIn/Scripts/FitInBackground.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class FitInBackground : MonoBehaviour {
 	[SerializeField] FitInObstacleManager fitInObstacleManager;
-	[SerializeField] Transform environmentObject;
-	[SerializeField] float repeatInterval = 50;
+	[SerializeField] Transform background1;
+	[SerializeField] Transform background2;
+	Vector3 background1Pos;
+	Vector3 background2Pos;
 
 	// Use this for initialization
 	void Start () {
-		
+		background1Pos = background1.transform.position;
+		background2Pos = background2.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		CheckSecondBackground();
 	}
 
 	void FixedUpdate()
 	{
 		var speed = Time.fixedDeltaTime * fitInObstacleManager.obstacleSpeedCurrent;
-		environmentObject.position += new Vector3(0, 0, -speed);
+		background1.position += new Vector3(0, 0, -speed);
+		background2.position += new Vector3(0, 0, -speed);
+	}
+
+	void CheckSecondBackground()
+	{
+		if (background2.transform.position.z <= background1Pos.z)
+		{
+			background1.transform.position = background1Pos;
+			background2.transform.position = background2Pos;
+		}
 	}
 
 
